433316d57b187dd95bbccfafd5c935649b142615
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-30 23:35:51 +0900

    트랜잭션 문제 해결 - 트랜잭션 AOP 적용

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,44 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 계좌이체 메서드
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 비즈니스 로직
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    // 예외 상황을 만들기 위한 메서드
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
