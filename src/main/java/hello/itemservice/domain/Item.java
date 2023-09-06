13aafca9616c0f76ec6c43989d3513497936d513
김경렬 <rlarudfuf802@naver.com>
2023-09-06 17:06:44 +0900

    JPA 적용1 - 개발

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -1,16 +1,30 @@
 package hello.itemservice.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity // JPA 객체
+//@Table(name = "item") // 객체 명이랑 같으면 생략 가능
 public class Item {
 
+    // 해당 필드를 테이블의 PK와 매핑
+    @Id
+    // DB에서 PK 값 생성하는 방식을 사용 ex) AUTO_INCREMENT
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 해당 필드를 테이블의 컬럼과 매핑(이름이 같으면 생략 가능)
+    // 스프링부트와 통합해서 사용하면 필드명을 테이블 컬럼명으로 변환할 때,
+    // 카멜 케이스를 언더스코어로 자동 변환해준다!
+    // 즉, 여기서 굳이 name = "item_name" 지정을 안해줘도 된다는 것!!
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // JPA는 public 또는 protected의 기본 생성자가 필수로 있어야한다.
+    // 프록시 기술을 쓰기 편하게 한다...
     public Item() {
     }
 
---
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -0,0 +1,91 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Transactional // JPA에서 데이터를 변경할 때는 항상 트랜잭션을 사용한다.
+public class JpaItemRepository implements ItemRepository {
+
+    private final EntityManager em;
+
+    public JpaItemRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item); // 저장
+
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item item = em.find(Item.class, itemId);
+
+        item.setItemName(updateParam.getItemName());
+        item.setPrice(updateParam.getPrice());
+        item.setQuantity(updateParam.getQuantity());
+        // 트랜잭션이 커밋되는 시점에 변경 감지를 해서 업데이트 쿼리를 날리고 커밋한다.
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+
+        return Optional.ofNullable(item);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String jpql = "select i from Item i";
+
+        Integer maxPrice = cond.getMaxPrice();
+        String itemName = cond.getItemName();
+
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            jpql += " where";
+        }
+
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            jpql += " i.itemName like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+
+        if (maxPrice != null) {
+            if (andFlag) {
+                jpql += " and";
+            }
+
+            jpql += " i.price <= :maxPrice";
+        }
+
+        log.info("jpql={}", jpql);
+
+        TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+
+        if (StringUtils.hasText(itemName)) {
+            query.setParameter("itemName", itemName);
+        }
+
+        if (maxPrice != null) {
+            query.setParameter("maxPrice", maxPrice);
+        }
+
+        return query.getResultList();
+    }
+}
